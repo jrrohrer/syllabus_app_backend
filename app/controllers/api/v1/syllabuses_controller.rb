434730c96d802e8c2c0a3c48283626ca789b85a5
jrrohrer <jessie.rohrer@gmail.com>
2021-07-22 17:59:10 -0400

    add create action to syllabus controller

--- app/controllers/api/v1/syllabuses_controller.rb
+++ app/controllers/api/v1/syllabuses_controller.rb
@@ -1,2 +1,25 @@
 class Api::V1::SyllabusesController < ApplicationController
+
+  def index
+    syllabuses = Syllabus.all
+    render json: syllabuses
+  end
+
+  def create
+    syllabus = Syllabus.new(syllabus_params)
+    if syllabus.save
+      # if the object is good, render the data and send an 'accepted' status m
+      render json: syllabus, status: :accepted 
+    else
+      # if object doesn't save, render an error 
+      render json: {errors: syllabus.errors.full_messages}, status: :unprocessible_entity
+    end
+  end
+
+  private
+
+  def syllabus_params
+    params.require(:syllabus).permit(:title, :description, :image_url, :category_id)
+  end
+
 end
